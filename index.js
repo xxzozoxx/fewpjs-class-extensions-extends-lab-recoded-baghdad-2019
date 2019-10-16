8b968e94caf1b8c8c461d297598cf6de721aa5d7
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-16 19:16:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,14 @@ class Polygon {
 class Triangle extends Polygon {
   get isValid (){
     if (this.arr.length !== 3) return;
+    let side1 = this.arr[0]
+    let side2 = this.arr[1]
+    let side3 = this.arr[2]
+    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+  }
+}
+class Square extends Polygon {
+  get isValid(){
+    if(this.arr.length !== 4) return;
   }
 }
